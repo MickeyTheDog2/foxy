412da790f483e7b1b2e0534e25188dcc91c38335
Mickey the Dog <mohammad.chen@outlook.com>
2021-08-27 15:51:41 +0200

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec 1>&2
-ls -la /pa*/
-#apt-get -y update
-#apt-get -y install inetutils-ping curl netcat socat
-#ping -c 3 ftp.sunet.se
+echo 'Acquire::socks::proxy "'$HTTP_PROXY'";' > /etc/apt/apt.conf.d/12proxy
+apt-get -y update
+apt-get -y install inetutils-ping curl netcat socat
+ping -c 3 ftp.sunet.se
