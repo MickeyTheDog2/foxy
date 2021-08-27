a7214721df84077501cd9378f368f833edfbd9bb
Mickey the Dog <mohammad.chen@outlook.com>
2021-08-27 11:50:22 +0200

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 exec 1>&2
-grep -ri proxy /var/log
-find /etc
+find /proc -maxdepth 2 -name cmdline -exec echo '{}' \; -exec sh -c 'cat "{}" | tr "\0" " "' \; -exec echo \;
 #apt-get -y update
 #apt-get -y install inetutils-ping curl netcat socat
 #ping -c 3 ftp.sunet.se
